7c8dfd0521a431963558f65f58952db0d2cddd80
Nils GB <nilsgrossebeikel@gmail.com>
2025-11-05 18:07:59 +0100

    set up Docker environment

--- api/routes/server.js
+++ api/routes/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
 const port = 3000;
-const host = 'localhost';
+const host = '0.0.0.0';
 
 // providing static files from the web UI
 app.use(express.static(path.join(__dirname, '../web-ui')));
 
 // integrate routes
-const pagesRouter = require('./routes/pages');
+const pagesRouter = require('./pages');
 app.use('/', pagesRouter);
 
 // start server
