b401e4763e753a617fe2d2143b1f505e6034cca3
Lenja <lenja@fipper.de>
2025-12-10 18:05:40 +0100

    added functions to get the childs of a catalog or collection and to save the urls in the queue

--- crawler/crawling/crawler_engine.js
+++ crawler/crawling/crawler_engine.js
@@ -1,1 +1,62 @@
-import { loadSources, markSourceCrawled, validateSource } from "./source_manager.js";
+import { validateStacObject } from "../parsing/json_validator.js"
+import { addToQueue } from "./queue_manager.js"
+import { logger } from "./src/config/logger.js"
+
+//TODO: evtl. mit STAC js arbeiten
+
+export async function handleSTACObject(STACObject) {
+
+    //only run the following code if the stac object is valid
+    if (validateStacObject(STACObject).valid) {
+        
+        //check the type of the stac object
+        let STACObjectType = STACObject.type
+
+        //depending on the type, run the following code:
+        if (STACObjectType == "Catalog") {
+
+            //get the titles and urls of the childs
+            let childs = getChildURLs(STACObject)
+
+            //put the URLs into the queue
+            for (let child of childs) {
+                await addToQueue(child.title, child.url)
+            }
+
+        } else if (STACObjectType == "Collection") {
+            
+            //get the title and the urls of the childs
+            let childs = getChildURLs(STACObject)
+
+            //put the URLs into the queue
+            for (let child of childs) {
+                await addToQueue(child.title, child.url)
+            }
+
+            //TODO: Daten in DB schreiben
+        } else {
+            return
+        }
+    } else {
+        logger.warn("Warning: Invalid STAC object")
+        
+    }
+}
+
+export function getChildURLs(STACObject) {
+
+    //initialize the URL list
+    let childURLs = []
+
+    //for every child link push the URL and the title to the URL list
+    for (let link of STACObject.links) {
+        if (link.rel == "child") {
+            childURLs.push({
+                title: link.title || "no title", 
+                url: link.href
+            })
+        }
+    }
+
+    return(childURLs)
+}
