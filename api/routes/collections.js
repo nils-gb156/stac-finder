68fd1800d6c8a14ef8b0e699575d214d5ba5c236
Luis Waldeyer <l.waldeyer@uni-muenster.de>
2025-12-14 19:30:02 +0100

    adding /health endpoint for service monitoring

--- api/routes/collections.js
+++ api/routes/collections.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getCollections, getCollectionById } = require('../controllers/collections');
+const { getQueryables } = require('../controllers/queryables');
+
+router.get('/queryables', getQueryables);
 
 router.get('/', getCollections);
+
 router.get('/:id', getCollectionById);
 
 module.exports = router;
