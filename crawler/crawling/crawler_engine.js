4bb037f58e853612425e05e5d440b6d3031b56a3
Lenja <lenja@fipper.de>
2025-12-23 21:57:28 +0100

    added functions to get the data from the stac index database

--- crawler/crawling/crawler_engine.js
+++ crawler/crawling/crawler_engine.js
@@ -4,11 +4,13 @@ import {
     initializeQueue,
     getNextUrlFromDB,
     hasNextUrl,
-    removeFromQueue
+    removeFromQueue,
+    addToQueue
 } from "./queue_manager.js";
 import { handleSTACObject } from "./crawler_functions.js"
 import { validateStacObject } from "../parsing/json_validator.js";
 import { logger } from "./src/config/logger.js"
+import { getSTACIndexData } from "../data_management/stac_index_client.js";
 
 /**
 * Main crawler loop:
@@ -29,6 +31,14 @@ export async function startCrawler() {
 
     // Initialize queue from database
     await initializeQueue();
+
+    // get the urls from the STAC Index db
+    const STACIndexData = await getSTACIndexData();
+    
+    //push the urls to the queue
+    for (let data of STACIndexData) {
+        await addToQueue(data.title, data.url);
+    }
 
     // Continue crawling until no URLs remain in queue
     while (await hasNextUrl()) {
