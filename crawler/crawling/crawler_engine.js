67926581fba9c4b9a8b164b9a71b025db31dbbf7
Lenja <lenja@fipper.de>
2026-01-12 08:38:43 +0100

    added function to continue crawling

--- crawler/crawling/crawler_engine.js
+++ crawler/crawling/crawler_engine.js
@@ -42,30 +42,89 @@ export async function startCrawler() {
 
     // Continue crawling until no URLs remain in queue
     while (await hasNextUrl()) {
-        
+
         // Fetch next URL entry from queue table
-        const entry = await getNextUrlFromDB();
-        const url = entry.url_of_source;
-        const parentUrl = entry.parent_url ?? null;
+            const entry = await getNextUrlFromDB();
+            const url = entry.url_of_source;
+            const parentUrl = entry.parent_url ?? null;
 
-        //get the json from the url
-        const res = await fetch(url)
-        const STACObject = await res.json()
+        try {
+            //get the json from the url
+            const res = await fetch(url)
+            const STACObject = await res.json()
 
-        console.log(`Crawling: ${url}`);
+            console.log(`Crawling: ${url}`);
+            
+            // Only proceed if valid JSON was retrieved
+            if (validateStacObject(STACObject).valid) {
+
+                //for Catalogs: put the child urls into the queue
+                //for Collections: put the child urls into the queue, save the data in the sources/collections db
+                await handleSTACObject(STACObject, url, parentUrl)
         
-        // Only proceed if valid JSON was retrieved
-        if (validateStacObject(STACObject).valid) {
-
-            //for Catalogs: put the child urls into the queue
-            //for Collections: put the child urls into the queue, save the data in the sources/collections db
-            await handleSTACObject(STACObject, url, parentUrl)
-    
-            } else {
-                logger.warn("Warning: Invalid STAC object")
-            }
+                } else {
+                    logger.warn("Warning: Invalid STAC object")
+                }
+            
+        } catch(err) {
+            logger.warn(`Warning: Did not crawled the following url: ${url} because of the following error: ${err}`)
+        }
         
-            // Remove processed URL from queue to avoid re-processing
+        // Remove processed URL from queue to avoid re-processing
+        await removeFromQueue(url);
+    }
+
+    console.log("Crawling finished");
+}
+
+/**
+* Continue crawler-loop:
+* continues crawling if the crawling process stopped because of a network failiure
+* - repeatedly fetches the URLs that are already in the queue
+* - adds new URLs into queue
+* - removes processed URL
+*
+* This function implements the core recursive traversal described in the Pflichtenheft.
+*
+* @async
+* @function continueCrawlingProcess
+* @returns {Promise<void>} Completes when no URLs remain in the queue
+*/
+export async function continueCrawlingProcess() {
+
+    console.log("Crawling Process starts again where it stopped");
+
+    // Continue crawling until no URLs remain in queue
+    while (await hasNextUrl()) {
+
+        // Fetch next URL entry from queue table
+            const entry = await getNextUrlFromDB();
+            const url = entry.url_of_source;
+            const parentUrl = entry.parent_url ?? null;
+
+        try {
+            //get the json from the url
+            const res = await fetch(url)
+            const STACObject = await res.json()
+
+            console.log(`Crawling: ${url}`);
+            
+            // Only proceed if valid JSON was retrieved
+            if (validateStacObject(STACObject).valid) {
+
+                //for Catalogs: put the child urls into the queue
+                //for Collections: put the child urls into the queue, save the data in the sources/collections db
+                await handleSTACObject(STACObject, url, parentUrl)
+        
+                } else {
+                    logger.warn("Warning: Invalid STAC object")
+                }
+            
+        } catch(err) {
+            logger.warn(`Warning: Did not crawled the following url: ${url} because of the following error: ${err}`)
+        }
+        
+        // Remove processed URL from queue to avoid re-processing
         await removeFromQueue(url);
     }
 
