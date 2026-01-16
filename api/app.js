5ff9dab4a7efec8231970bb4309e7d56a3119647
Nils GB <nilsgrossebeikel@gmail.com>
2026-01-16 15:15:23 +0100

    feat: remove api html landing page

--- api/app.js
+++ api/app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 const collectionsRouter = require('./routes/collections');
 const healthRouter = require('./routes/health');
-const pagesRouter = require('./routes/pages');
 const queryablesRouter = require('./routes/queryables');
 const { morganMiddleware, requestLogger } = require('./middleware/logger');
 const { errorHandler, notFoundHandler } = require('./middleware/errorHandler');
@@ -15,11 +14,7 @@ app.use(requestLogger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Static files from Web UI
-app.use(express.static(path.join(__dirname, '../web-ui')));
-
 // Routes
-app.use('/', pagesRouter);
 app.use('/collections', collectionsRouter);
 app.use('/collections/queryables', queryablesRouter);
 app.use('/health', healthRouter);
