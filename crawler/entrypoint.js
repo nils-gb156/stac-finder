22a1108821d9704f4e55c2b521e3a7d87efe6ab5
Lenja <lenja@fipper.de>
2026-01-21 15:59:28 +0100

    changed entrypoint

--- crawler/entrypoint.js
+++ crawler/entrypoint.js
@@ -1,8 +1,8 @@
 import { startCrawler } from "./crawling/crawler_engine.js";
 import { saveInBackup } from "./crawling/queue_manager.js";
+import { logger } from "./crawling/src/config/logger.js"
 
-startCrawler()
-
+//if someone stops the programm, save the data that is not yet uploaded lacally
 process.on('SIGINT', () => {
   saveInBackup()
   process.exit(0)
@@ -12,3 +12,15 @@ process.on('SIGTERM', () => {
   saveInBackup()
   process.exit(0)
 })
+
+try {
+  // start crawling process
+  startCrawler()
+} catch(err) {
+  // if there is a error
+  // save not uploaded data
+  saveInBackup()
+
+  // log the error
+  logger.error(err)
+}
