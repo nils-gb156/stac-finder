4a809d00baf434046d4fd1c3c0044d0e8ab27c3b
Cedric Gräber <cedric.graeber@gmx.de>
2026-01-28 10:39:30 +0100

    Previous crawling start

--- crawler/data_management/db_client.js
+++ crawler/data_management/db_client.js
@@ -5,20 +5,34 @@
 
 import pkg from "pg"
 const { Pool } = pkg
-import 'dotenv/config'
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
+let pool;
 
-pool.on('error', (err) => {
-    console.error('Unexpected DB error:', err);
-    process.exit(-1);
-});
+function getPool() {
+    if (!pool) {
+        pool = new Pool({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME
+        });
+
+        pool.on('error', (err) => {
+            console.error('Unexpected DB error:', err);
+            process.exit(-1);
+        });
+
+        console.log("DB ENV CHECK:", {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME
+        });
+    }
+    return pool;
+}
 
 export const query = (text, params) => pool.query(text, params);
 export { pool };
