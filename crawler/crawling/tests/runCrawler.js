ec6d59f9f51b2f5c714b17deac4e21f85efe89ff
Cedric Gräber <cedric.graeber@gmx.de>
2026-01-28 10:39:30 +0100

    Previous crawling start

---
+++ crawler/crawling/tests/runCrawler.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+    path: path.resolve(__dirname, "../../.env")
+});
+
+// MUST be after dotenv.config()
+import { startCrawler } from "../crawler_engine.js";
+
+console.log("ENV CHECK:", {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+await startCrawler();
